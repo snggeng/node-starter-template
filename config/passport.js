df09c23f1458944f73b35be046cef05c810b0175
Geng <snggeng@gmail.com>
2017-06-14 09:31:19 +0800

    new commit

--- config/passport.js
+++ config/passport.js
@@ -1,93 +1,131 @@
 
 // Load passport local
-var localStrategy = require('passport-local').Strategy;
+var localStrategy = require('passport-local').Strategy
+var facebookStrategy = require('passport-facebook').Strategy
 
 // Load validator
-var validator = require('validator');
+var validator = require('validator')
 
 // Load user model
-var User = require('../model/user');
+var User = require('../model/user')
 
-module.exports = function( passport ) {
-
+module.exports = function (passport) {
   // Serialize user
-  passport.serializeUser( function( user, done){
-      done(null, user.id);
-  });
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  })
 
   // Deserialize user
-  passport.deserializeUser(function(id, done){
-      User.findById(id, function(err, user){
-        done(err, user);
-      });
-  });
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user)
+    })
+  })
 
   // Passport signup
   passport.use('local-signup', new localStrategy({
-      usernameField : 'email',
-      passwordField : 'password',
-      passReqToCallback: true
-    },
-    function( req, email, password, done){
-
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+    function (req, email, password, done) {
         // Check that the email is in the right format
-        if( !validator.isEmail(email) ){
-          return done(null, false, req.flash('loginMessage','That is not a valid email address'));
-        }
+      if (!validator.isEmail(email)) {
+        return done(null, false, req.flash('loginMessage', 'That is not a valid email address'))
+      }
 
         // Check that the password is at least 8 chars
-        if( password.length < 8 ){
-          return done(null, false, req.flash('loginMessage','The password needs to be 8 chars long'));
-        }
+      if (password.length < 8) {
+        return done(null, false, req.flash('loginMessage', 'The password needs to be 8 chars long'))
+      }
 
-        process.nextTick(function(){
-          User.findOne( {'local.email' : email }, function(err, user){
-            if(err){
-              return done(err);
-            }
-            if(user){
-              return done(null, false, req.flash('loginMessage','That email is already in use'));
-            }else{
-              var newUser = new User();
-              newUser.local.email = email;
-              newUser.local.password = password;
-              newUser.save(function(err){
-                if(err){
-                  console.log(err);
-                }
-                return done(null, newUser, req.flash('loginMessage', 'Logged in successfully'));
-              });
-            }
-          });
-        });
-    }));
+      process.nextTick(function () {
+        User.findOne({ 'local.email': email }, function (err, user) {
+          if (err) {
+            return done(err)
+          }
+          if (user) {
+            return done(null, false, req.flash('loginMessage', 'That email is already in use'))
+          } else {
+            var newUser = new User()
+            newUser.local.email = email
+            newUser.local.password = password
+            newUser.save(function (err) {
+              if (err) {
+                console.log(err)
+              }
+              return done(null, newUser, req.flash('loginMessage', 'Logged in successfully'))
+            })
+          }
+        })
+      })
+    }))
 
   // Passport login
   passport.use('local-login', new localStrategy({
-      usernameField : 'email',
-      passwordField : 'password',
-      passReqToCallback: true
-    },
-    function( req, email, password, done){
-        process.nextTick(function(){
-          User.findOne( {'local.email' : email }, function(err, user){
-            if(err){
-              return done(err);
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+    function (req, email, password, done) {
+      process.nextTick(function () {
+        User.findOne({ 'local.email': email }, function (err, user) {
+          if (err) {
+            return done(err)
+          }
+
+          if (!user) {
+            return done(null, false, req.flash('loginMessage', 'sorry no one by that email'))
+          }
+
+          user.validPassword(password, function (err, isMatch) {
+            if (err) {
+              return console.log(err)
+            }
+            if (isMatch) {
+              return done(null, user, req.flash('loginMessage', 'Logged in successfully'))
             }
 
-            if(!user){
-              return done(null,false, req.flash('loginMessage', 'sorry no one by that email'));
-            }
+            return done(null, false, req.flash('loginMessage', 'sorry wrong password'))
+          })
+        })
+      })
+    }))
 
-            user.validPassword(password, function(err, isMatch){
+    // Passport facebook login
+  passport.use('facebook', new facebookStrategy({
+    clientID: '120436848543276',
+    clientSecret: 'c6c45b296cfaa2f7a2d2625eeb67c483',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['name', 'email', 'link', 'locale', 'timezone'],
+    passReqToCallback: true
+  },
+  function (req, accessToken, refreshToken, profile, done) {
+    console.log(profile)
 
-              if(isMatch){
-                return done(null, user, req.flash('loginMessage', 'Logged in successfully'));
-              }
+    User.findOne({ 'facebook.id': profile.id }, function (err, user) {
+      if (err) {
+        return done(err)
+      }
 
-              return done(null,false, req.flash('loginMessage', 'sorry wrong password'));
-            })
-          });
-        });
-    }));
+      if (!user) {
+        var newUser = new User()
+        newUser.facebook.id = profile.id
+        newUser.facebook.token = accessToken
+        newUser.facebook.name = profile.first_name + ' ' + profile.middle_name + ' ' + profile.last_name
+        newUser.facebook.email = profile.email
+        newUser.save(function (err) {
+          if (err) {
+            console.log(err)
+          }
+          return done(null, newUser, req.flash('loginMessage', 'Logged in successfully'))
+        })
+      }
+
+      if (user) {
+        return done(null, user, req.flash('loginMessage', 'Logged in successfully'))
+      }
+    })
+  }
+))
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,46 +1,52 @@
-module.exports = function(app, passport){
-
+module.exports = function (app, passport) {
   // Routes
   // router middelware
-  function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-      return next();
+  function isLoggedIn (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
     }
     res.redirect('/')
   }
 
   // Sign up
-  app.get('/', function(req, res){
-    res.render('signup', { message: req.flash('loginMessage') });
-  });
+  app.get('/', function (req, res) {
+    res.render('signup', { message: req.flash('loginMessage') })
+  })
 
   // Sign up
   app.post('/', passport.authenticate('local-signup', {
-    successRedirect : '/secret',
-    failureRedirect : '/',
+    successRedirect: '/secret',
+    failureRedirect: '/',
     failureFlash: true
-  }));
+  }))
 
   // Login
-  app.get('/login', function(req, res){
-    res.render('login', { message: req.flash('loginMessage') });
-  });
+  app.get('/login', function (req, res) {
+    res.render('login', { message: req.flash('loginMessage') })
+  })
 
   // Login
   app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/secret',
-    failureRedirect : '/login',
+    successRedirect: '/secret',
+    failureRedirect: '/login',
     failureFlash: true
-  }));
+  }))
 
     // Secret
-  app.get('/secret', isLoggedIn, function(req, res){
-    res.render('secret', { message: req.flash('loginMessage') });
-  });
+  app.get('/secret', isLoggedIn, function (req, res) {
+    res.render('secret', { message: req.flash('loginMessage') })
+  })
 
   // logout
-  app.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
+  app.get('/logout', function (req, res) {
+    req.logout()
+    res.redirect('/')
+  })
+
+  // facebook login
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }))
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
+    res.redirect(req.session.returnTo || '/')
+  })
 }
